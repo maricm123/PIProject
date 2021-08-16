f60ace4e7716b26b5c7c59a6b7fc16f944f4cfd2
ivans1999 <stankovicivan1999@gmail.com>
2021-08-16 16:20:51 +0200

    Inicijalni komit

---
+++ ProjekatPI/src/main/java/ftn/magacinsko/ProjekatPI/ProjekatPiApplication.java
@@ -0,0 +1,13 @@
+package ftn.magacinsko.ProjekatPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatPiApplication.class, args);
+	}
+
+}
---
+++ ProjekatPI/src/test/java/ftn/magacinsko/ProjekatPI/ProjekatPiApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.magacinsko.ProjekatPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
