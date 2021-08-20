90d871836d4c958e3b3909cec5e76485fa210548
ivans1999 <stankovicivan1999@gmail.com>
2021-08-20 21:52:53 +0200

    pocetak rada na roba ili usluga cont

--- ProjekatPI/src/main/java/ftn/magacinsko/controller/RobaIliUslugaController.java
+++ ProjekatPI/src/main/java/ftn/magacinsko/controller/RobaIliUslugaController.java
@@ -1,5 +1,26 @@
 package ftn.magacinsko.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ftn.magacinsko.entityDTO.RobaIliUslugaDTO;
+import ftn.magacinsko.serviceInterface.RobaIliUslugaServiceInterface;
+
+@RestController
+@RequestMapping(value = "api/roba-ili-usluga")
 public class RobaIliUslugaController {
 
+	@Autowired
+	private RobaIliUslugaServiceInterface robaIliUslugaServiceInterface;
+	
+	@GetMapping
+	public ResponseEntity<List<RobaIliUslugaDTO>> getRobeIliUsluge(){
+		
+		return ResponseEntity.ok().body(robaIliUslugaServiceInterface.findAll());
+	}
 }
