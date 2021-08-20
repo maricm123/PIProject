d596864622da06afacff92b663da1602c0b64b16
ivans1999 <stankovicivan1999@gmail.com>
2021-08-20 21:51:19 +0200

    dovrsen magacin controller.

--- ProjekatPI/src/main/java/ftn/magacinsko/controller/MagacinController.java
+++ ProjekatPI/src/main/java/ftn/magacinsko/controller/MagacinController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +37,32 @@ public class MagacinController {
 	public ResponseEntity<MagacinDTO> getMagacin(@PathVariable("id") Integer id){
 		
 		return ResponseEntity.ok().body(magacinServiceInterface.findOne(id));
+	}
+	@GetMapping(value = "preduzece/{id}")
+	public ResponseEntity<List<MagacinDTO>> getMagaciniByPreduzece(@PathVariable("id") Integer id){
+		
+		return ResponseEntity.ok().body(magacinServiceInterface.findByPreduzece_id(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<MagacinDTO> addMagacin(@RequestBody MagacinDTO magacinDTO){
+		
+		return ResponseEntity.ok().body(magacinServiceInterface.save(magacinDTO));
+	}
+
+	@PutMapping(value = "/{id}", consumes = "application/json")
+	public ResponseEntity<Void> updateMagacin(@RequestBody MagacinDTO magacinDTO, @PathVariable("id") Integer id){
+		try {
+			magacinServiceInterface.update(id,magacinDTO);
+			return  ResponseEntity.noContent().build();
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteMagacin(@PathVariable("id") Integer id){
+		magacinServiceInterface.remove(id);
+		return  ResponseEntity.noContent().build();
 	}
 }
