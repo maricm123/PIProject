22cd83bc29905bc56f4a44bfd96c2fa2cfb6125a
ivans1999 <stankovicivan1999@gmail.com>
2021-08-23 19:57:01 +0200

    uradjen mag.kartica controller

--- ProjekatPI/src/main/java/ftn/magacinsko/controller/MagacinskaKarticaController.java
+++ ProjekatPI/src/main/java/ftn/magacinsko/controller/MagacinskaKarticaController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,28 @@ public class MagacinskaKarticaController {
 	public ResponseEntity<List<MagacinskaKarticaDTO>> getMagacinskeKartice(){
 		
 		return ResponseEntity.ok().body(magaKarticaServiceInterface.findAll());
+	}
+	
+	@GetMapping(value = "{sifraMagacina}/{sifraRobeIliUsluge}/{brojPoslovneGodine}")
+	public ResponseEntity<List<MagacinskaKarticaDTO>> getMagKartByRobaIliUslua(@PathVariable("sifraMagacina") Integer sifraMagacina, @PathVariable("sifraRobeIliUsluge") Integer sifraRobeIliUsluge, 
+			@PathVariable("brojPoslovneGodine") Integer brojPoslovneGodine){
+		
+		return ResponseEntity.ok().body(magaKarticaServiceInterface.findByRobaIliUsluga_sifraAndPoslovnaGodina_brojGodineAndMagacin_sifraMagacina(sifraRobeIliUsluge, brojPoslovneGodine, sifraMagacina));
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<MagacinskaKarticaDTO> getOneMagKar(@PathVariable("id") Integer id){
+		return ResponseEntity.ok().body(magaKarticaServiceInterface.findOneById(id));
+	}
+	
+	@PutMapping
+	public ResponseEntity<MagacinskaKarticaDTO> nivelacija(@RequestBody MagacinskaKarticaDTO dto){
+		
+		return ResponseEntity.ok().body(magaKarticaServiceInterface.nivelacija(dto));
+	}
+	
+	@GetMapping(value = "/report/{idMagacinskeKartice}")
+	public ResponseEntity getReport(@PathVariable("idMagacinskeKartice") String idMagacinskeKartice){
+		return magaKarticaServiceInterface.report(idMagacinskeKartice);
 	}
 }
