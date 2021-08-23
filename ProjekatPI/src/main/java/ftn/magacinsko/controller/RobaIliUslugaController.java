a402e09fdcb10ed7ef3914e54703a622af2d2e38
ivans1999 <stankovicivan1999@gmail.com>
2021-08-23 20:00:33 +0200

    uradjen roba ili usluga controller

--- ProjekatPI/src/main/java/ftn/magacinsko/controller/RobaIliUslugaController.java
+++ ProjekatPI/src/main/java/ftn/magacinsko/controller/RobaIliUslugaController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +27,37 @@ public class RobaIliUslugaController {
 	public ResponseEntity<List<RobaIliUslugaDTO>> getRobeIliUsluge(){
 		
 		return ResponseEntity.ok().body(robaIliUslugaServiceInterface.findAll());
+	}
+	@GetMapping(value = "/{sifraMagacina}")
+	public ResponseEntity<List<RobaIliUslugaDTO>> getRobeIliUslugeByMagacin(@PathVariable("sifraMagacina") Integer sifraMagacina){
+		
+		return ResponseEntity.ok().body(robaIliUslugaServiceInterface.findByMagKartica(sifraMagacina));
+	}
+	
+	@GetMapping(value = "jedna-roba-ili-usluga/{id}")
+	public ResponseEntity<RobaIliUslugaDTO> getRiliL(@PathVariable("id") Integer id){
+		
+		return ResponseEntity.ok().body(robaIliUslugaServiceInterface.findOneBySifra(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<RobaIliUslugaDTO> addRobaIliUsluga(@RequestBody RobaIliUslugaDTO dto){
+		return ResponseEntity.ok().body(robaIliUslugaServiceInterface.save(dto));
+	}
+	
+	@PutMapping(value = "/{sifra}", consumes = "application/json")
+	public ResponseEntity<Void> updateRobaIliUsluga(@RequestBody RobaIliUslugaDTO robaIliUslugaDTO, @PathVariable("sifra") Integer sifra){
+		try {
+			robaIliUslugaServiceInterface.update(sifra,robaIliUslugaDTO);
+			return  ResponseEntity.noContent().build();
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteRobaIliUsluga(@PathVariable("id") Integer id){
+		robaIliUslugaServiceInterface.delete(id);
+		return  ResponseEntity.noContent().build();
 	}
 }
